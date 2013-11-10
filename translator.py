153190465865eb8dbbcca346412b49ec90e14664
FJSingel <fjs52@case.edu>
2013-11-10 18:27:12 -0500

    Appears to work now. Testing will begin.

--- translator.py
+++ translator.py
@@ -9,45 +9,63 @@ import re
 import sys
 
 #Regex Strings
-number = "\d\d\d\d\d\d\d"
-upper = "[A-Z]+"
-user = "[a-z]+"
-F13 = "F13"
-S13 = "S13"
+regexes = ["\d\d\d\d\d\d\d", 
+			"[A-Z]+",
+			"[A-Z]+",
+			"[A-Z]*",
+			"[a-z]+",
+			"F13",
+			"S[0-9]{2}"]
 
-has_middle = False
+def main():
+	for line in sys.stdin:
+		tokens = re.split("\s", line)
+		#remove empty tokens
+		tokens = filter(None, tokens)
 
-for line in sys.stdin:
-	tokens = re.split("\s", line)
-	#remove empty tokens
-	tokens = filter(None, tokens)
+		try:
+			#Ensures entry has 7 values
+			if len(tokens) == 6:
+				tokens.insert(3, "")
+			elif len(tokens) != 7:
+				raise ValueError
 
-	try:
-		validate_tokens(tokens)
-	except ValueError:
-		print "Data does not match."
-	else:
-		print "Unexpected error:", sys.exc_info()[0]
-    	raise
+			_validate_tokens(tokens)
+		except ValueError:
+			print "Data does not match."
+
+	    #Form a line for printing
+		line = "".join([token + ',' for token in tokens])
+		print line[:-1]
+
+
+def _validate_tokens(tokens):
+	token_pairs = zip(regexes, tokens)
+	for regex, token in token_pairs:
+		if(re.match(regex, token) == None):
+			raise ValueError
+
+if __name__ == '__main__':
+	main()
 
 #Refactor this
-def check_tokens(self, tokens):
-	if(len(tokens) != 6 or len(tokens) != 7):
-		raise ValueError
-	if(re.match(number, tokens[0]) == None):
-		raise ValueError
-	if(re.match(upper, tokens[1]) == None):
-		raise ValueError
-	if(re.match(upper, tokens[2]) == None):
-		raise ValueError
-	if(re.match(upper, tokens[3] == None)):
-		has_middle = False
-	else:
-		has_middle = True
-	if(has_middle):
-		#Token should be the nickname
-		pass
-	else:
-		#Token at 3 should be userID
-		if(re.match(user, tokens[3] == None)):
-			has_middle = False
+# def check_tokens(self, tokens):
+# 	if(len(tokens) != 6 or len(tokens) != 7):
+# 		raise ValueError
+# 	if(re.match(number, tokens[0]) == None):
+# 		raise ValueError
+# 	if(re.match(upper, tokens[1]) == None):
+# 		raise ValueError
+# 	if(re.match(upper, tokens[2]) == None):
+# 		raise ValueError
+# 	if(re.match(upper, tokens[3] == None)):
+# 		has_middle = False
+# 	else:
+# 		has_middle = True
+# 	if(has_middle):
+# 		#Token should be the nickname
+# 		pass
+# 	else:
+# 		#Token at 3 should be userID
+# 		if(re.match(user, tokens[3] == None)):
+# 			has_middle = False
